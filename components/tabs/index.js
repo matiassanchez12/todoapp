11a22fdb53b47cadeb1d40a8ebd6549f8ac75472
Matias <matias.sanchez.1897@hotmail.com>
2022-04-02 17:38:29 -0300

    other ch43

--- components/tabs/index.js
+++ components/tabs/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Tab, TabList, TabPanel, TabPanels, Tabs as TabsChakra } from "@chakra-ui/react";
+import { Box, Stack, Tab, TabList, TabPanel, TabPanels, Tabs as TabsChakra } from "@chakra-ui/react";
 import List from "../list";
 import Calendar from "../calendar";
 import GeneralNotes from "../notes/General";
@@ -12,39 +12,50 @@ function Tabs() {
   React.useEffect(() => onChange(new Date()), []);
 
   return (
-    <Stack
-      flexDirection="row"
-      flexFlow="row wrap"
-      bg="blue.800"
-      justifyContent="center"
-      gap={4}
-      paddingInline={6}
-      flex={1}
-      alignItems="start"
-      marginTop="0px !important"
-    >
-      <Stack p={4} flex={1} h="100%" alignItems="center" justifyContent="center">
-        <Calendar value={value} onChange={onChange} />
+    <Box w="100%" marginTop="0px !important">
+      <Stack
+        bg="blue.800"
+        flexFlow="row wrap"
+        flexDirection="row"
+        justifyContent="center"
+        gap={4}
+        flex={1}
+        alignItems="start"
+        w="100%"
+      >
+        <Stack p={4} flex={1} w="100%" h="100%" alignItems="center" justifyContent="center">
+          <Calendar value={value} onChange={onChange} />
+        </Stack>
+        <TabsChakra
+          p={5}
+          variant="solid-rounded"
+          w="100%"
+          minH={480}
+          align="center"
+          display="flex"
+          flex={2}
+          flexDirection="column"
+          gap={6}
+        >
+          <TabList>
+            <Tab>TAREAS</Tab>
+            <Tab>NOTAS DEL DIA</Tab>
+            <Tab>NOTAS GENERALES</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <List selectedDay={value} />
+            </TabPanel>
+            <TabPanel>
+              <DailyNotes selectedDay={value} listCards={listCards} setListCards={setListCards} />
+            </TabPanel>
+            <TabPanel>
+              <GeneralNotes selectedDay={value} listCards={listCards} setListCards={setListCards} />
+            </TabPanel>
+          </TabPanels>
+        </TabsChakra>
       </Stack>
-      <TabsChakra p={5} variant="solid-rounded" minH={480} align="center" display="flex" flex={2} flexDirection="column" gap={6}>
-        <TabList>
-          <Tab>TAREAS</Tab>
-          <Tab>NOTAS DEL DIA</Tab>
-          <Tab>NOTAS GENERALES</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <List selectedDay={value} />
-          </TabPanel>
-          <TabPanel>
-            <DailyNotes selectedDay={value} listCards={listCards} setListCards={setListCards} />
-          </TabPanel>
-          <TabPanel>
-            <GeneralNotes selectedDay={value} listCards={listCards} setListCards={setListCards} />
-          </TabPanel>
-        </TabPanels>
-      </TabsChakra>
-    </Stack>
+    </Box>
   );
 }
 
